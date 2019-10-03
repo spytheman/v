aa73f028f1efc497361a4297c19b7f8bccb48909
Delyan Angelov <delian66@gmail.com>
2019-10-03 11:09:07 +0300

    Fix vlib/orm/orm.v optional return value declaration for rows_first_or_empty .

--- vlib/orm/orm.v
+++ vlib/orm/orm.v
@@ -5,7 +5,7 @@ pub mut:
 	vals []string
 }
 
-pub fn rows_first_or_empty(rows []orm.Row) orm.Row? {
+pub fn rows_first_or_empty(rows []orm.Row) ?orm.Row {
 	if rows.len == 0 { return error('no rows') } 
 	return rows[0]
 }
