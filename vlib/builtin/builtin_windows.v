46be4530be5dcc542fc3ad27b3b7dfe05ae68559
Delyan Angelov <delian66@gmail.com>
2019-11-09 19:06:19 +0200

    Add file and line information to the backtraces too.

--- vlib/builtin/builtin_windows.v
+++ vlib/builtin/builtin_windows.v
@@ -21,11 +21,18 @@ pub struct SymbolInfo {
 	f_max_name_len       u32
 	f_name               byte
 }
-
 pub struct SymbolInfoContainer {
 	pub mut:
 	syminfo SymbolInfo
 	f_name_rest [254]char
+}
+
+pub struct Line64 {
+	f_size_of_struct  u32
+	f_key             voidptr
+	f_line_number     u32
+	f_file_name       byteptr
+	f_address         u64
 }
 
 fn C.SymSetOptions(symoptions u32) u32 // returns the current options mask
@@ -33,6 +40,7 @@ fn C.GetCurrentProcess() voidptr // returns handle
 fn C.SymInitialize(h_process voidptr, p_user_search_path byteptr, b_invade_process int) int
 fn C.CaptureStackBackTrace(frames_to_skip u32, frames_to_capture u32, p_backtrace voidptr, p_backtrace_hash voidptr) u16
 fn C.SymFromAddr(h_process voidptr, address u64, p_displacement voidptr, p_symbol voidptr) int
+fn C.SymGetLineFromAddr64(h_process voidptr, address u64, p_displacement voidptr, p_line &Line64) int
 
 const (
 	SYMOPT_DEBUG = 0x80000000
@@ -41,38 +49,50 @@ const (
 )
 
 fn print_backtrace_skipping_top_frames_msvc(skipframes int){
+	mut offset := u64(0) 
+	backtraces := [100]voidptr
+	sic := SymbolInfoContainer{} 
+	mut si := &sic.syminfo
+	si.f_size_of_struct = sizeof(SymbolInfo)  // Note: C.SYMBOL_INFO is 88
+	si.f_max_name_len = sizeof(SymbolInfoContainer) - sizeof(SymbolInfo) - 1
+	fname := *char( &si.f_name )
+	mut sline64 := Line64{}
+	sline64.f_size_of_struct = sizeof(Line64)
+
 	handle := C.GetCurrentProcess()
+	defer{ C.SymCleanup(handle) }
+
 	options := C.SymSetOptions(SYMOPT_DEBUG | SYMOPT_LOAD_LINES | SYMOPT_UNDNAME)	
 	syminitok := C.SymInitialize( handle, 0, 1)
 	if syminitok != 1 {
 		println('Failed getting process: Aborting backtrace.\n')
 		return
 	}
-	mut sic := SymbolInfoContainer{} 
-	mut si := &sic.syminfo
-	si.f_size_of_struct = sizeof(SymbolInfo)  // Note: C.SYMBOL_INFO is 88
-	si.f_max_name_len = sizeof(SymbolInfoContainer) - sizeof(SymbolInfo) - 1
-	fname := *char( &si.f_name )
-
-	backtraces := [100]voidptr
 	frames := int( C.CaptureStackBackTrace(skipframes + 1, 100, backtraces, 0) )
 	for i:=0; i < frames; i++ {
 		// fugly pointer arithmetics follows ...
 		s := *voidptr( u64(backtraces) + u64(i*sizeof(voidptr)) )
-		symfa_ok := C.SymFromAddr( handle, *s, 0, si )
-		if (symfa_ok == 1) {
+		symfa_ok := C.SymFromAddr( handle, *s, &offset, si )
+		if symfa_ok == 1 {
 			nframe := frames - i - 1
-			println('${nframe:-2d}: NameLen: ${si.f_name_len:3d} | ' + tos3(fname) )
+			mut lineinfo := ''
+			symglfa_ok := C.SymGetLineFromAddr64(handle, *s, &offset, &sline64)
+			if symglfa_ok == 1 {
+				lineinfo = ' ${sline64.f_file_name}:${sline64.f_line_number}'
+			}else{
+				//cerr := int(C.GetLastError()) println('SymGetLineFromAddr64 failure: $cerr ')
+			}
+			sfunc := tos3(fname)
+			println('${nframe:-2d}: ${sfunc:-25s} $lineinfo')
 		} else {
 			// https://docs.microsoft.com/en-us/windows/win32/debug/system-error-codes
 			cerr := int(C.GetLastError())
 			println('SymFromAddr failure: $cerr (Note: 87 = The parameter is incorrect)')  // 87 = The parameter is incorrect.
-		} 
+		}
 	}
-	C.SymCleanup(handle)
 }
 
 fn print_backtrace_skipping_top_frames_mingw(skipframes int){
-	println('TODO: print_backtrace_skipping_top_frames_mingw $skipframes')
+	println('TODO: print_backtrace_skipping_top_frames_mingw($skipframes)')
 }
 
