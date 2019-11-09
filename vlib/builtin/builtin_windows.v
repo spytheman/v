b5ed53ea9ce90985728cec0aafbacb2060235d25
Delyan Angelov <delian66@gmail.com>
2019-11-09 18:09:37 +0200

    Fixed backtrace for msvc on windows .

--- vlib/builtin/builtin_windows.v
+++ vlib/builtin/builtin_windows.v
@@ -4,10 +4,10 @@ module builtin
 #flag windows -l dbghelp
 
 pub struct SymbolInfo {
-pub mut:
+	pub mut:
 	f_size_of_struct     u32
 	f_type_index         u32      // Type Index of symbol
-	f_reserved           voidptr
+	f_reserved           [2]u64
 	f_index              u32
 	f_size               u32
 	f_mod_base           u64      // Base Address of module comtaining this symbol
@@ -19,11 +19,20 @@ pub mut:
 	f_tag                u32      // pdb classification
 	f_name_len           u32      // Actual length of name
 	f_max_name_len       u32
-	f_name               byteptr     // CHAR[] Name of symbol
+	f_name               byte
 }
 
-fn C.SymSetOptions(u32) u32
-fn C.GetCurrentProcess() voidptr
+pub struct SymbolInfoContainer {
+	pub mut:
+	syminfo SymbolInfo
+	f_name_rest [254]char
+}
+
+fn C.SymSetOptions(symoptions u32) u32 // returns the current options mask
+fn C.GetCurrentProcess() voidptr // returns handle
+fn C.SymInitialize(h_process voidptr, p_user_search_path byteptr, b_invade_process int) int
+fn C.CaptureStackBackTrace(frames_to_skip u32, frames_to_capture u32, p_backtrace voidptr, p_backtrace_hash voidptr) u16
+fn C.SymFromAddr(h_process voidptr, address u64, p_displacement voidptr, p_symbol voidptr) int
 
 const (
 	SYMOPT_DEBUG = 0x80000000
@@ -33,67 +42,37 @@ const (
 
 fn print_backtrace_skipping_top_frames_msvc(skipframes int){
 	println('print_backtrace_skipping_top_frames_msvc $skipframes')
-	print_backtrace_skipping_top_frames_win(skipframes)
-}
-
-fn print_backtrace_skipping_top_frames_mingw(skipframes int){
-	println('print_backtrace_skipping_top_frames_mingw $skipframes')
-	print_backtrace_skipping_top_frames_win(skipframes)
-}
-
-
-
-fn print_backtrace_skipping_top_frames_win(skipframes int){
-	stack := [100]byteptr
-	//		stack := [100]u64
 	handle := C.GetCurrentProcess()
-	println( 'HANDLE: $handle')
-
-	//options := C.SymSetOptions(C.SYMOPT_DEBUG | C.SYMOPT_LOAD_LINES | C.SYMOPT_UNDNAME | C.SYMOPT_ALLOW_ZERO_ADDRESS | C.SYMOPT_CASE_INSENSITIVE)
-	options := C.SymSetOptions(SYMOPT_DEBUG | SYMOPT_LOAD_LINES | SYMOPT_UNDNAME)
-	println('options= ${options}')
-
-	//		mut success := C.SymInitialize(handle, C.NULL, 1)
-	mut success := C.SymInitialize(C.GetCurrentProcess(), 0, 1)
-	println('SymInitialize Success= ${int(success)} handle= ${handle} = ${C.GetCurrentProcess()}')
-
-	if (success != 1) {
+	options := C.SymSetOptions(SYMOPT_DEBUG | SYMOPT_LOAD_LINES | SYMOPT_UNDNAME)	
+	syminitok := C.SymInitialize( handle, 0, 1)
+	if syminitok != 1 {
 		println('Failed getting process: Aborting backtrace.\n')
 		return
 	}
+	mut sic := SymbolInfoContainer{} 
+	mut si := &sic.syminfo
+	si.f_size_of_struct = sizeof(SymbolInfo)  // Note: C.SYMBOL_INFO is 88
+	si.f_max_name_len = sizeof(SymbolInfoContainer) - sizeof(SymbolInfo) - 1
+	fname := *char( &si.f_name )
 
-	frames := C.CaptureStackBackTrace(0, 100, stack, 0)
-	println('Nb frames= ${int(frames)}')
-
-	println('Sizes of Symbol_Info= ${sizeof(SymbolInfo)}  string= ${sizeof(string)}')
-
-	for i:=0; i < frames; i++
-	{
-		println(' Stack[ ${i.str()} ] = "${u64(stack[i])}"')
-
-		mut symbol_info := SymbolInfo{}
-		symbol_info.f_size_of_struct = sizeof(SymbolInfo) // Note: C.SYMBOL_INFO is 88
-		symbol_info.f_max_name_len= 1024
-		symbol_info.f_name = calloc(1024)
-
-		// https://docs.microsoft.com/en-us/windows/win32/api/dbghelp/nf-dbghelp-symfromaddr
-	//			success = C.SymFromAddr(C.GetCurrentProcess(), u64(stack[i]), 0, &symbol_info)
-		success = C.SymFromAddr(C.GetCurrentProcess(), u64(stack[i]), 0, &symbol_info)
-
-		if (success == 1) {
-			println('   SymFromAddr success: ${int(success)} ')
-			println('        SizeOfStruct: ${symbol_info.f_size_of_struct}')
-			println('        Address: $symbol_info.f_address')
-			println('        NameLen: ' + ptr_str(symbol_info.f_name_len))
-			println('        Name: ' + ptr_str(symbol_info.f_name))
-		}
-	  	else {
+	backtraces := [100]voidptr
+	frames := int( C.CaptureStackBackTrace(skipframes + 1, 100, backtraces, 0) )
+	for i:=0; i < frames; i++ {
+		// fugly pointer arithmetics follows ...
+		s := *voidptr( u64(backtraces) + u64(i*sizeof(voidptr)) )
+		symfa_ok := C.SymFromAddr( handle, *s, 0, si )
+		if (symfa_ok == 1) {
+			println('NameLen: ${si.f_name_len:3d} | ' + tos3(fname) )
+		} else {
 			// https://docs.microsoft.com/en-us/windows/win32/debug/system-error-codes
-			println('   SymFromAddr failure: ${C.GetLastError()}  (Note: 87 = The parameter is incorrect)')  // 87 = The parameter is incorrect.
+			cerr := int(C.GetLastError())
+			println('   SymFromAddr failure: $cerr (Note: 87 = The parameter is incorrect)')  // 87 = The parameter is incorrect.
 		} 
 	}
-
 	C.SymCleanup(handle)
+}
 
+fn print_backtrace_skipping_top_frames_mingw(skipframes int){
+	println('TODO: print_backtrace_skipping_top_frames_mingw $skipframes')
 }
 
