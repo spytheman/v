bfb7ead2c94be04c3a5068e4316a822c6c5fead3
Delyan Angelov <delian66@gmail.com>
2019-12-25 13:19:20 +0200

    Add some asserts. Fix unneeded println string interpolation.

--- vlib/ftp/ftp_test.v
+++ vlib/ftp/ftp_test.v
@@ -1,46 +1,59 @@
 module main
 
-
 import ftp
 
 fn test_all() {
+	mut fails := 0
 	mut ftp := ftp.new()
-	ftp.debug()
-
+	//ftp.debug()
+	
 	// ftp.rediris.org
 	if ftp.connect('ftp.redhat.com') { 
 		println("connected")
-
+		assert true
+		
 		if ftp.login('ftp','ftp') {
 			println('logged-in')
-
+			assert true
+			
 			pwd := ftp.pwd()
 			println('pwd: $pwd')
-
+			assert true
+			
 			ftp.cd('/')
-
+			
 			data := ftp.dir() or {
-				println('cannot list folder')
+				eprintln('cannot list folder')
+				assert false
 				return
 			}	
-			println('$data')
-
+			println(data)
+			
 			ftp.cd('/suse/linux/enterprise/11Server/en/SAT-TOOLS/SRPMS/')
-				
+			
 			dir_list := ftp.dir() or {
-				println('cannot list folder')
+				eprintln('cannot list folder')
+				assert false
 				return
 			}
 			println('$dir_list')
-
+			assert true
+			
 			blob := ftp.get('katello-host-tools-3.3.5-8.sles11_4sat.src.rpm') or {
-				println("couldn't download it")
+				eprintln("couldn't download it")
+				assert false
 				return
 			}
-
+			
 			println('downloaded $blob.len bytes')
+			assert blob.len == 55670
+		}else{
+			fails++
 		}
-
+		
 		ftp.close()
+	}else{
+		fails++
 	}
+	assert fails == 0
 }
