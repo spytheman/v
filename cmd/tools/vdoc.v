c9b90d097d7cdf6d35152b938ba7aee3fb073062
Delyan Angelov <delian66@gmail.com>
2020-02-20 11:50:59 +0200

    Add an usage screen to 'v doc' .

--- cmd/tools/vdoc.v
+++ cmd/tools/vdoc.v
@@ -12,8 +12,25 @@ import (
 
 fn main(){
 	args := cmdline.options_after(os.args, ['doc'])
-	for mod in args {
-		table := table.new_table()
-		println(doc.doc(mod, table))
+	if args.len != 1 {
+		usage()
+		exit(0)
 	}
+	mod := args[0]
+	table := table.new_table()
+	println(doc.doc(mod, table))
+}
+
+fn usage(){
+	print('Usage: 
+a) v doc <module>
+   Show documentation for the given module.
+
+b) [wip] v doc
+   Show documentation for current module (the one in the working folder).
+
+c) [wip] v doc <ModuleName> <SymbolName>
+   Show documentation *only* for the specific SymbolName from module ModuleName. 
+   SymbolName is a module top level entity like a function, a const, an enum, a struct, a type and so on.
+')
 }
