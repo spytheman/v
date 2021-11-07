fdf7b869f2afc91b0ef424662e2008bec0e8fff7
Delyan Angelov <delian66@gmail.com>
2021-11-07 04:20:27 +0200

    gx: add an init() runtime deprecation message

---
+++ vlib/gx/deprecated.v
@@ -0,0 +1,7 @@
+module gx
+
+fn init() {
+	eprintln('NB: the `gx` module is deprecated, and will be removed after 2021/12/07.')
+	eprintln('All its consts, types, functions are moved into `gg`')
+	eprintln('Please `import gg` and replace `gx.` with `gg.` in your source files.')
+}
