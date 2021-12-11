ced7bb10784f20eaeeb14a56def59c71738de884
Delyan Angelov <delian66@gmail.com>
2021-12-11 18:13:02 +0200

    ci: fix the failing `ubuntu-tcc` task

--- vlib/v/gen/c/comptime.v
+++ vlib/v/gen/c/comptime.v
@@ -36,7 +36,7 @@ fn (mut g Gen) comptime_call(mut node ast.ComptimeCall) {
 	}
 	if node.method_name == 'env' {
 		// $env('ENV_VAR_NAME')
-		val := util.cescaped_path(os.getenv(node.args_var))
+		val := util.cescaped_path(os.getenv(node.args_var.clone()))
 		g.write('_SLIT("$val")')
 		return
 	}
