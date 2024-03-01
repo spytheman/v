066b5b80b94b8d887f37a295586d2bb189f9677e
Delyan Angelov <delian66@gmail.com>
2024-03-01 04:51:20 +0200

    remove the trace_cgen_on_leave_scope check

--- vlib/v/gen/c/scoped_defer.v
+++ vlib/v/gen/c/scoped_defer.v
@@ -13,9 +13,7 @@ enum LeaveScopeKind {
 }
 
 fn (mut g Gen) on_leave_current_scope(kind LeaveScopeKind, pos token.Pos) {
-	$if trace_cgen_on_leave_scope ? {
-		g.writeln('\t// ${g.scope_leaves:5} on_leave_current_scope ${kind:-13}, line_nr: ${
-			pos.line_nr + 1:5}, pos: ${pos.pos:5}, ...${g.file.path#[-30..]:30}')
-	}
+	g.writeln('\t// ${g.scope_leaves:5} on_leave_current_scope ${kind:-13}, line_nr: ${
+		pos.line_nr + 1:5}, pos: ${pos.pos:5}, ...${g.file.path#[-30..]:30}')
 	g.scope_leaves++
 }
