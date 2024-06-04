acb3d05667c9a27207adad3821e0face520a5c4b
Delyan Angelov <delian66@gmail.com>
2024-06-04 08:47:15 +0300

    working draft on using VString on the Ruby side

--- examples/call_v_from_ruby/test.rb
+++ examples/call_v_from_ruby/test.rb
@@ -19,6 +19,24 @@ def shared_library_extension
   end
 end
 
+class VString < FFI::Struct
+  layout :str, :pointer,
+         :len, :int,
+         :is_lit, :int
+  def initialize(mystring)
+    s = mystring.to_s
+    pointer = FFI::MemoryPointer.from_string(s.to_str.encode(Encoding::UTF_8))
+    self[:str] = pointer
+    self[:len] = pointer.size
+  end
+  def to_ss()
+    print "-------------------- :str ", self[:str], "\n"
+    print "-------------------- :len ", self[:len], "\n"
+    print "-----------------------------------------------\n"
+    self[:str].read_string_length(self[:len])
+  end
+end
+
 module Lib
   extend FFI::Library
 
@@ -30,7 +48,7 @@ module Lib
 
   attach_function :square, [:int], :int
   attach_function :sqrt_of_sum_of_squares, [:double, :double], :double
-  attach_function :process_v_string [:string], :string
+  attach_function :process_v_string, [VString.val], VString.val
 end
 
 puts "Lib.square(10) result is #{Lib.square(10)}"
@@ -39,5 +57,14 @@ raise 'Cannot validate V square().' unless Lib.square(10) == 100
 raise 'Cannot validate V sqrt_of_sum_of_squares().' unless \
   Lib.sqrt_of_sum_of_squares(1.1, 2.2) == Math.sqrt(1.1*1.1 + 2.2*2.2)
 
-puts "Lib.process_v_string('hi') result is #{Lib.process_v_string('hi')}"
-  
+##pointer = FFI::MemoryPointer.from_string('hi'.to_str.encode(Encoding::UTF_8))
+inp = VString.new("hi")
+puts inp
+puts inp.to_ss
+puts "------------------------------------------------ calling --------------------------------------"
+res = Lib.process_v_string(VString.new("hi"))
+puts "------------------------------------------------ V function called -------------------------------------"
+puts res
+puts res.to_ss
+puts "done"
+
--- examples/call_v_from_ruby/test.v
+++ examples/call_v_from_ruby/test.v
@@ -5,16 +5,35 @@ import math
 
 @[export: 'square']
 fn square(i int) int {
-	return i * i
+        return i * i
 }
 
 @[export: 'sqrt_of_sum_of_squares']
 fn sqrt_of_sum_of_squares(x f64, y f64) f64 {
-	return math.sqrt(x * x + y * y)
+        return math.sqrt(x * x + y * y)
+}
+
+// you do not have to use the same name in the export attribute
+@[export: 'process_v_string_cstrings']
+fn work(input &char) &char {
+        s := unsafe { input.vstring_literal() }
+        eprintln(s.len)
+        eprintln(s)
+        // println(s.is_lit)
+        return 'v ${s} v'.str
 }
 
 // you do not have to use the same name in the export attribute
 @[export: 'process_v_string']
-fn work(s string) string {
-	return 'v ${s} v'
+fn work2(s string) string {
+        ps := unsafe { voidptr(&s) }
+        eprintln('> v      &s: ${ps}')
+        eprintln('> v   s.str: ${voidptr(s.str)}')
+        eprintln('> v   s.len: ${voidptr(s.len)}')
+        res := 'v ${s} v'
+        pres := unsafe{ voidptr(&res) }
+        eprintln('> v    &res: ${pres}')
+        eprintln('> v res.str: ${voidptr(res.str)}')
+        eprintln('> v res.len: ${voidptr(res.len)}')
+        return res
 }
