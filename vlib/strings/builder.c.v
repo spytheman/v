ace75394d21789d02e8ea942ef839496a5759c4f
Delyan Angelov <delian66@gmail.com>
2025-12-12 11:52:46 +0200

    strings: fix the backwards compatibility that the remaining shim provides (use an alias of an alias)

--- vlib/strings/builder.c.v
+++ vlib/strings/builder.c.v
@@ -1,13 +1,14 @@
 module strings
 
 // Builder is used to efficiently append many strings to a large buffer.
+
 @[deprecated: 'use the builtin StringBuilder instead']
 @[deprecated_after: '2026-09-12']
-pub type Builder = []u8
+pub type Builder = StringBuilder
 
 // new_builder returns a new string builder, with an initial capacity of `initial_size`.
 @[deprecated: 'use the builtin `new_string_builder(cap: size)` instead']
 @[deprecated_after: '2026-09-12']
-pub fn new_builder(initial_size int) StringBuilder {
-   return new_string_builder(cap: initial_size)
+pub fn new_builder(initial_size int) Builder {
+	return new_string_builder(cap: initial_size)
 }
